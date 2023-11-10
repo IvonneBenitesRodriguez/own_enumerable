992068c604b155743f672f395f069bd032927c91
Ivonne <ivonnebenites2015@gmail.com>
2023-11-09 19:41:31 -0500

    Update file

--- mylist.rb
+++ mylist.rb
@@ -2,15 +2,14 @@ require_relative 'my_enumerable'
 
 class MyList
   include MyEnumerable
-   def initialize(*items)
-   @list = items
+  def initialize(*items)
+    @list = items
   end
 
   def each(&block)
-
-  @list.each(&block)
-   end
- end
+    @list.each(&block)
+  end
+end
 
 list = MyList.new(1, 2, 3, 4)
 puts(list.all? { |e| e < 5 })
